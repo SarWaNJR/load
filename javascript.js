eab215fd98c83385e24d8b78abd83edc99b0943a
SarWaNJR <93028248+SarWaNJR@users.noreply.github.com>
2023-02-20 17:44:01 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
-let burger = document.querySelector('.burger');
-let Links = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    Links.classList.toggle('showthis');
+// Variables declare
+let a = document.querySelector('.burger');
+let b = document.querySelector('.nav-links');
+let textArea = document.querySelector('.text-content');
+a.addEventListener('click', () => {
+    b.classList.toggle('showthis');
+    textArea.classList.toggle('show');
 })
